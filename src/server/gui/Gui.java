98c5a7917645e5c1166b468204c9d71050ecfdc5
joonas <ceo@varisware.com>
2015-07-25 00:31:23 +0300

    .

--- src/server/gui/Gui.java
+++ src/server/gui/Gui.java
@@ -6,7 +6,7 @@ import java.text.SimpleDateFormat;
  * Created by joonas on 24.7.2015.
  */
 public abstract class Gui {
-    protected final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("DD/MM/YYYY|HH:mm:ss");
+    protected final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd/MM/YYYY|HH:mm:ss");
 
     public abstract void printInfo(String info);
     public abstract void printError(String error, Exception e);
--- src/server/util/Sender.java
+++ src/server/util/Sender.java
@@ -19,12 +19,14 @@ public class Sender {
     public static void broadcastToAll(List<ConnectionHandler> clients, String type, String text) {
         MessageObject message = new MessageObject(type, text, SENDER_NAME);
         for (int i = 0; i < clients.size(); i++) {
+            Server.getGui().printInfo("Sending: " + text);
             clients.get(i).getOutput().printf(Server.gson.toJson(message));
         }
     }
 
     public static void closeConnection(ConnectionHandler client, String message) {
         try {
+            Server.getGui().printInfo("Sending: " + message);
             MessageObject object = new MessageObject(ServerConstants.REQUEST_DISCONNECT, message, SENDER_NAME);
             client.getOutput().printf(Server.gson.toJson(object));
             client.disconnectFromClient();
